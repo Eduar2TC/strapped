da4d5f2a70ce5c1a2c250b070e73d36b6804f43e
eduar2tc@gmail.com <eduar2tc@gmail.com>
2022-02-19 04:25:47 -0600

    init

---
+++ js/main.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var typed = new Typed('.aside-tex', {
+        strings: ['<span class="aside-text-style text-primary">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>', '<span class="aside-text-style">Nam sint saepe repellat officiis eum vel.</span>'],
+        loop: true,
+        typeSpeed: 40,
+        backSpeed: 30,
+    });
+});
+/*Collapses */
+window.onload = function(){
+    /*Headers*/
+    const flushheading1 = document.getElementById('flush-headingOne');
+    const headerOffset = flushheading1.offsetHeight;
+    /*Bodys */
+    const flushCollapse1 = document.getElementById('flush-collapseOne');
+    const flushCollapse2 = document.getElementById('flush-collapseTwo');
+    const flushCollapse3 = document.getElementById('flush-collapseThree');
+    const flushCollapse4 = document.getElementById('flush-collapseFour');
+    const flushCollapse5 = document.getElementById('flush-collapseFive');
+    const flushCollapse6 = document.getElementById('flush-collapseSix');
+    const flushCollapse7 = document.getElementById('flush-collapseSeven');
+    const flushCollapse8 = document.getElementById('flush-collapseEight');
+    const flushCollapse9 = document.getElementById('flush-collapseNine');
+
+    const flushes = [
+        flushCollapse1,
+        flushCollapse2,
+        flushCollapse3,
+        flushCollapse4,
+        flushCollapse5,
+        flushCollapse6,
+        flushCollapse7,
+        flushCollapse8,
+        flushCollapse9
+    ];
+    
+    let collapses = [];
+    const box = document.querySelectorAll('.services .box');
+    for( let i = 0; i < flushes.length; i++){
+        collapses[i] = new bootstrap.Collapse(flushes[i], {
+            toggle: false
+        });
+        box[i].addEventListener('click', function(e) {
+            const h5 = box[i].querySelector('h5');
+            let bx = document.querySelectorAll('.box-active');
+            let h5Active = document.querySelectorAll('.h5-active');
+                bx.forEach(function (item) {
+                    item.classList.remove("box-active");
+                });
+                h5Active.forEach(function (item) {
+                    item.classList.remove("h5-active");
+                });
+            box[i].classList.add('box-active');
+            h5.classList.add('h5-active');
+            collapses[i].toggle();
+            flushes[i].scrollIntoView({
+                block: 'center',
+                behavior: "smooth" 
+            });
+        });
+    }
+};
